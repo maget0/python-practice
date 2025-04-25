d27f1d12c2d525ea6185e977af6c4cf6951954e2
maget0 <jason.mageto@strathmore.edu>
2025-04-25 21:45:14 +0300

    Add files via upload

--- Sum.py
+++ Sum.py
@@ -8,3 +8,7 @@ def sum_all (*args):
 
 print(sum_all(1,2,3,4,5))
 print(sum_all(4.5, 1.3, 2.2, 1.7, 0.9))
+
+numbers=[2,4,6,8,10,12]
+print(sum_all(*numbers))
+#The asterisk symbol "*" is used to unpack elements in a list/array
